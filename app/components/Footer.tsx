22ada4ab885320f16ba7110c1553166545b97aea
bharatpestcontrol107 <bharatpestservice@gmail.com>
2026-06-27 12:58:31 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -73,7 +73,7 @@ export default function Footer() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span className="text-sm">Serving all of Birmingham</span>
+                <span className="text-sm">9TH FLOOR, UNIT NO. 906, TECHNO IT PARK, LINK ROAD, BORIVALI WEST, Mumbai, Mumbai Suburban, Maharashtra, 400092</span>
               </li>
             </ul>
           </div>
